5bb40b07ca7c2517ef13246e0a1c34fc38f0e57a
Ryan M Allen <rmallen09@yahoo.com>
2019-04-09 13:45:40 -0600

    Updated my barplots

--- tidy_tuesday_4-8.R
+++ tidy_tuesday_4-8.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(data.table)
 
 player_dob <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/player_dob.csv")
 
@@ -19,31 +20,39 @@ top_10m <- slice(males, 1:10)
 
 females <- as.data.table(filter(g, gender == "Female") %>% arrange(desc(rolling_win_count)))
 top_10f <- slice(females, 1:10)
-                                                        
-ggplot(top_10m, aes(x=reorder(name, table(rolling_win_count)[rolling_win_count]), y = rolling_win_count)) + 
-    geom_bar(stat = 'identity')
 
-ggplot(top_10m,
-       aes(x=reorder(name, name,
-                     function(x)-length(x)) , y = rolling_win_count)) + geom_bar(stat = 'identity')
 
 top_10m$name <- factor(top_10m$name, levels = top_10m$name[order(-top_10m$rolling_win_count)])
 
 chart <- ggplot(top_10m, aes(x=name, y= rolling_win_count))
 chart + geom_bar(stat='identity') + theme(panel.grid.major = element_blank(), axis.ticks = element_blank(), panel.grid.minor = element_blank(),
-                   panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-    geom_text(aes(label=rolling_win_count), vjust=-0.5) + ggtitle("Top 10 Male Tennis Players")
+                                          panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  geom_text(aes(label=rolling_win_count), vjust=-0.5) + ggtitle("Top 10 Male Tennis Players")
 
 top_10f$name <- factor(top_10f$name, levels = top_10f$name[order(-top_10f$rolling_win_count)])
 
 chart1 <- ggplot(top_10f, aes(x=name, y= rolling_win_count))
 chart1 + geom_bar(stat='identity') + theme(panel.grid.major = element_blank(), axis.ticks = element_blank(), panel.grid.minor = element_blank(),
-                                          panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-    geom_text(aes(label=rolling_win_count), vjust=-0.5) + ggtitle("Top 10 Female Tennis Players")
+                                           panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  geom_text(aes(label=rolling_win_count), vjust=-0.5) + ggtitle("Top 10 Female Tennis Players")
 
+top_10f$rank <- 1:10
+top_10m$rank <- 1:10
 tops <- rbind(top_10f, top_10m)
+tops$rank <- NULL
 
-p <- ggplot(tops, aes(name, rolling_win_count)) + geom_point()
+chart1 + geom_bar(stat='identity') + theme(panel.grid.major = element_blank(), axis.ticks = element_blank(), panel.grid.minor = element_blank(),
+                                           panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  geom_text(aes(label=rolling_win_count), vjust=-0.5) + ggtitle("Top 10 Female Tennis Players") +
+  geom_bar(data = top_10m, aes(x=name, y= rolling_win_count), stat = 'identity')
+
+
+
+p <- ggplot(tops, aes(name, rolling_win_count)) + geom_bar(stat = 'identity')
 
 # Use vars() to supply variables from the dataset:
-p + facet_grid(rows = vars(tops$gender))
+p + facet_grid(cols = vars(tops$gender))
+
+
+
+https://stackoverflow.com/questions/21349329/drawing-a-barchart-to-compare-two-sets-of-data-using-ggplot2-package
